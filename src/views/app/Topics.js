ab4f4948e39f2a5e8fefc310028cfc42eccedfee
SimeonAlexandrov <simeon.alexandrov17@gmail.com>
2019-09-01 21:14:15 +0300

    Added a more detailed Topics page.

--- src/views/app/Topics.js
+++ src/views/app/Topics.js
@@ -5,24 +5,74 @@ import {
   ScrollView, 
   StyleSheet, 
   Dimensions,
-  Text
+  Text,
+  Platform
 } from 'react-native'
 
-
-const SCREEN_WIDTH = Dimensions.get('window').width
+import { Button, ListItem, Input, Icon, ButtonGroup } from "react-native-elements" 
+import TouchableScale from 'react-native-touchable-scale'
 
 class Topics extends Component {
   constructor(props) {
     super(props)
 
-    this.state = {}
+    this.state = {
+        topics: ["JavaScript", "Sth else"]
+    }
   }
 
   render() {
     return (
       <ScrollView style={styles.container} keyboardShouldPersistTaps="handled">
         <View style={styles.contentView}>
-          <Text>This is where you will pick topics</Text>
+          <Text>Add topics of interest</Text>
+          <View style={{margin: 10}}>
+              <Input/>
+          </View>
+          <View>
+              <Button title="New" icon={
+                  <Icon
+                  name="plus"
+                  size={20}
+                  iconStyle={{
+                    color: "white"
+                  }}
+                  type="font-awesome"
+                />
+              }/>
+          </View>
+          <View style={{marginTop: 10, width: 200}}>
+              <Text>Popular topics:</Text>
+              <ButtonGroup
+                style={{minWidth: 200}}
+                buttons={["Angular", "React", "ML"]}
+              />
+          </View>
+          <View style={{margin: 10}}>
+              <Text>Your topics:</Text>
+              {
+                  this.state.topics.map(t => {
+                      return (
+                        <ListItem
+                            component={TouchableScale}
+                            friction={90}
+                            tension={100}
+                            activeScale={0.95}
+                            key={t}
+                            title={t}
+                            titleStyle={{ color: 'rgb(32, 137, 220)', fontWeight: 'bold' }}
+                            containerStyle={{
+                            marginHorizontal: 16,
+                            marginVertical: 8,
+                            borderRadius: 8,
+                            borderColor: "black",
+                            borderWidth: 2
+                            }}
+                        />
+                      )
+                  })
+              }
+          </View>
         </View>
       </ScrollView>
     )
@@ -37,7 +87,7 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: "5%",
     justifyContent: 'center',
-    alignItems: 'center',
+    alignItems: Platform.OS === "web" ? 'center' : 'stretch',
   }
 });
 
